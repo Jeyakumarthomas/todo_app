4e3d767c1dde43d2c3be9bce80d09a5eb227c5c3
Jeyakumarthomas <88367696+Jeyakumarthomas@users.noreply.github.com>
2025-07-28 11:55:47 +0530

    Project folders with todo component

---
+++ src/components/Todo.jsx
@@ -0,0 +1,44 @@
+import React,{useState} from 'react';
+import TodoItem from './TodoItem';
+
+const Todo = () => {
+  const [todeList,setTodoList] = useState([
+  {
+    id:123,
+    text:'readBooks',
+    isComplete:true
+  },
+  {
+    id:456,
+    text:'writingBooks',
+    isComplete:false
+  }
+]);
+
+  return (
+  <>
+    <div className="w-[30-rem]">
+      <h1 className = "text-lg my-2 font-medium text-amber-500">Todo list</h1>
+      <div className="flex gap-2">
+      <div className="flex-1">
+        <input type="text" name='' id='' placeholder='Add Your Task'
+        className="bg-white py-3 px-4 w-full border focus:outline-none focus:border-blue-500"/>
+      </div>
+      <button className='py-3 px-4 bg-blue-500 text-white hover:bg-blue-700 text-sm font-medium rounded-sm border-none'>Add Task</button>
+      </div>
+      <p className='my-3 text-sm text-zinc-400 px-1'>Fill Task Details</p>
+    </div>
+    <div className="w-[30-rem] bg-white shadow py-6 px-4">
+      <fieldset className='space-y-3'>
+        <legend className='text-pink-600 font-medium '>List of tasks</legend>
+        {/* list items */}
+        {/* <TodoItem text='Reading Books'/>}
+        
+        {/* list items */}
+      </fieldset>
+    </div>
+  </>
+  
+  )
+}
+export default Todo
