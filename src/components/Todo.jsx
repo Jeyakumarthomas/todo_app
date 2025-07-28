12602d36f6c18b683085153feae782c6aa8df932
Jeyakumarthomas <88367696+Jeyakumarthomas@users.noreply.github.com>
2025-07-28 12:25:38 +0530

    Task adding is compleated

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,8 @@
-import React,{useState} from 'react';
+import React,{useState,useRef} from 'react';
 import TodoItem from './TodoItem';
 
 const Todo = () => {
-  const [todeList,setTodoList] = useState([
+  const [todoList,setTodoList] = useState([
   {
     id:123,
     text:'readBooks',
@@ -15,16 +15,32 @@ const Todo = () => {
   }
 ]);
 
+const inputRef = useRef();
+const addTask = () => {
+  const inputText = inputRef.current.value.trim();
+  if(inputText===""){
+    return null;
+  }
+  const newTodo = {
+    id:Date.now(),
+    text:inputText,
+    isCompleted:false
+  }
+
+  setTodoList((prev)=>[...prev,newTodo])
+  inputRef.current.value = ""
+};
+
   return (
   <>
     <div className="w-[30-rem]">
       <h1 className = "text-lg my-2 font-medium text-amber-500">Todo list</h1>
       <div className="flex gap-2">
       <div className="flex-1">
-        <input type="text" name='' id='' placeholder='Add Your Task'
+        <input ref={inputRef} type="text" name='' id='' placeholder='Add Your Task'
         className="bg-white py-3 px-4 w-full border focus:outline-none focus:border-blue-500"/>
       </div>
-      <button className='py-3 px-4 bg-blue-500 text-white hover:bg-blue-700 text-sm font-medium rounded-sm border-none'>Add Task</button>
+      <button className='py-3 px-4 bg-blue-500 text-white hover:bg-blue-700 text-sm font-medium rounded-sm border-none' onClick={addTask}>Add Task</button>
       </div>
       <p className='my-3 text-sm text-zinc-400 px-1'>Fill Task Details</p>
     </div>
@@ -32,8 +48,8 @@ const Todo = () => {
       <fieldset className='space-y-3'>
         <legend className='text-pink-600 font-medium '>List of tasks</legend>
         {/* list items */}
-        {/* <TodoItem text='Reading Books'/>}
-        
+        {/* <TodoItem text='Reading Books'/>*/}
+        {todoList.length === 0?(<p classList="text-gray-500 text-sm">No task found</p>):(todoList.map((todo,index)=>{return <TodoItem text={todo.text} key={index} isComplete={todo.isComplete} id={todo.id}/>}))}
         {/* list items */}
       </fieldset>
     </div>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,7 +6,7 @@ const TodoItem = ({text, isComplete, id}) => {
   <label className={`hover:bg-slate-100 flex-1 p-2 rounded-md cursor-pointer select-none ${isComplete?"line-through text-slate-600":""}`}>{text}</label>   
   <div>
     <div className = "size-[26px] p-1 hover:bg-red-50 rounded-md">
-      <svg className="hover:fill-red-700 " xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+      <svg className="hover:fill-red-700 " xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="gray"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
     </div>
   </div>
 </div>
